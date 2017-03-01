9540adf9447cc9c0da4d1786c22d969c80614eb5
BHIMNATH <you@example.com>
2017-03-01 15:08:51 +0530

    Applet added

---
+++ AppletTest.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+/*<APPLET CODE="AppletTest.class" WIDTH="300" HEIGHT="400"></APPLET>*/
+public class AppletTest extends Applet{
+	public void paint(Graphics g){
+		g.drawString("Hello World",25,66);
+		//setBackground(Color.black);
+	}
+}
--- TestInterface.java
+++ TestInterface.java
@@ -24,4 +24,3 @@ public class TestInterface{
 		iface.message("Welcome");
 	}
 }
-
